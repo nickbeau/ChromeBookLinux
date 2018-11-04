5cadc645bab9bfc19763632140d35aafc6b8ee9a
Nick Beaugeard <nick@hubone.com>
2018-11-04 14:29:11 +1100

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -8,3 +8,10 @@ sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.1
+wget https://go.microsoft.com/fwlink/?LinkID=760868
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
