0044d28be95afe160308563f55d87b97792d8393
Nick Beaugeard <nick@hubone.com>
2018-11-04 14:22:42 +1100

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -2,7 +2,7 @@
 sudo apt-get install wget
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
 sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
-wget -q https://packages.microsoft.com/config/debian/8/prod.list
+wget -q https://packages.microsoft.com/config/debian/9/prod.list
 sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
